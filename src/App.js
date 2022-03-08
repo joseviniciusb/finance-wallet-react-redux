e0e35108257b6bb193822d658beaa16a2cf7ac9c
José Vinicius <josevbluz@outlook.com>
2022-03-08 16:41:21 -0300

    login page estructure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import Login from './pages/Login';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
-  return <div>Hello, TrybeWallet!!</div>;
+  return (
+    <>
+      <Router>
+        {/* A <Switch> looks through its children <Route>s and
+        renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+      
+    </>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,49 @@
 import React from 'react';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value,
+    });
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+
+    return (
+      <div className="loginContainer">
+        <form>
+          <label htmlFor="email">
+            User:
+            <input
+              name="email"
+              type="email"
+              data-testid="email-input"
+              onChange={ this.handleChange }
+              value={ email }
+            />
+          </label>
+          <label htmlFor="password">
+            Password:
+            <input
+              name="password"
+              type="password"
+              data-testid="password-input"
+              onChange={ this.handleChange }
+              value={ password }
+            />
+          </label>
+
+          <button type="button">Enviar</button>
+        </form>
+      </div>
+    );
   }
 }
 
