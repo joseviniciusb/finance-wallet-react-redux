181d3c95fde70b26d3ec3ca9079ccec3f843695b
José Vinicius <josevbluz@outlook.com>
2022-03-22 19:16:48 -0300

    edit button arrumando a logica um pouco

--- src/reducers/walletReducer.js
+++ src/reducers/walletReducer.js
@@ -6,25 +6,30 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
   let newId;
   let filteredExpenses;
+  let editedExpenses;
+  let foundExpenseIndex;
+
   switch (action.type) {
   case 'ADD_EXPENSE':
     if (state.expenses.length === 0) {
       newId = 0;
     } else {
-      newId = (state.expenses[state.expenses.length - 1].id * 1) + 1;
+      newId = state.expenses[state.expenses.length - 1].id * 1 + 1;
     }
     return {
       ...state,
       expenses: [...state.expenses, { id: newId, ...action.payload }],
     };
   case 'DELETE_EXPENSE':
-    filteredExpenses = state.expenses.filter((item) => item.id !== action.payload);
+    filteredExpenses = state.expenses.filter(
+      (item) => item.id !== action.payload,
+    );
     return { ...state, expenses: filteredExpenses };
   case 'EDIT_EXPENSE':
-    const editExpense = state.expenses.filter((item) => item.id === action.payload);
-    console.log(editExpense[0].value);
-    console.log({ ...state });
-    return { ...state, expenses: editExpense };
+    editedExpenses = [...state.expenses];
+    foundExpenseIndex = editedExpenses.findIndex((item) => (item.id === action.payload));
+    editedExpenses[foundExpenseIndex].value = 666;
+    return { ...state, expenses: editedExpenses };
   case 'UPDATE_CURRENCIES':
     return {
       ...state,
