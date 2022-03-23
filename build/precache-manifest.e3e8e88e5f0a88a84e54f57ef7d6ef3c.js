61f60594c2188825ceb454a589e700a08e0362f8
José Vinicius <josevbluz@outlook.com>
2022-03-23 15:24:27 -0300

    add build

---
+++ build/precache-manifest.e3e8e88e5f0a88a84e54f57ef7d6ef3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b04ed80abe1ef2ca7ff6089dcea054d6",
+    "url": "/trybe-wallet/index.html"
+  },
+  {
+    "revision": "22682b3838fe7428e7f3",
+    "url": "/trybe-wallet/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "38fea73ef0b865874dfd",
+    "url": "/trybe-wallet/static/js/2.6a56006e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybe-wallet/static/js/2.6a56006e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22682b3838fe7428e7f3",
+    "url": "/trybe-wallet/static/js/main.366ebc30.chunk.js"
+  },
+  {
+    "revision": "59d3c1ec903278203d08",
+    "url": "/trybe-wallet/static/js/runtime-main.2e2bea36.js"
+  }
+]);
